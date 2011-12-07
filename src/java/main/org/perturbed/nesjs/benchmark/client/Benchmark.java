d4eca63cfc782e3e317a79c2a9520b91591121fd
matt-preston <matt.r.preston@gmail.com>
2011-12-07 16:15:46 +0000

    Refactored out the test running duplication between the testcase & the benchmark

--- src/java/main/org/perturbed/nesjs/benchmark/client/Benchmark.java
+++ src/java/main/org/perturbed/nesjs/benchmark/client/Benchmark.java
@@ -1,11 +1,15 @@
 package org.perturbed.nesjs.benchmark.client;
 
-import java.io.*;
+import java.io.IOException;
 
-import org.perturbed.nesjs.core.client.*;
+import org.perturbed.nesjs.core.client.BlarggTestROM;
+import org.perturbed.nesjs.core.client.BlarggTestROM.TestLogger;
+import org.perturbed.nesjs.core.client.MOS6502;
+import org.perturbed.nesjs.core.client.Memory;
+import org.perturbed.nesjs.core.client.ROM;
 
-import com.google.gwt.core.client.*;
-import com.google.gwt.user.client.*;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 
 public class Benchmark implements EntryPoint
 {
@@ -64,99 +68,25 @@ public class Benchmark implements EntryPoint
     
     private void runTestROM(ROM aROM) throws IOException
     {
-        Memory _memory = aROM.toMemory();
+        BlarggTestROM _wrapper = new BlarggTestROM(aROM);
         
-        MOS6502 _6502 = new MOS6502(_memory);
-        
-        boolean _hasTestResetCPU = false;
-        
-        do
+        _wrapper.runTestToCompletion(new TestLogger() 
         {
-            _6502.step();
-            
-            if(isTestNeedsReset(_memory) && !_hasTestResetCPU)
-            {
-                _hasTestResetCPU = true;
-                
-                String _message = getNullTerminatedString(_memory, 0x6004);
-                System.out.println(_message);
-                
-                // Reset after 100ms, so just spin the CPU for a bit
-                for(int _index = 0; _index < 10000; _index++)
-                {
-                    _6502.step();
-                }
-                
-                _6502.reset();
-                System.out.println("RESET");
-            }            
-        }
-        while(!isTestFinished(_memory));
-        
-        String _message = getNullTerminatedString(_memory, 0x6004);
-        System.out.println(_message);
-        
-        /*
-        Assert.assertEquals(0x00, _memory.readByte(0x6000));
-        Assert.assertTrue(_message.contains("Passed"));
-        */
-    }
-
-//-----------------------------------
-//Private methods
-//-----------------------------------     
-    
-    private boolean isTestNeedsReset(Memory aMemory)
-    {
-        if(isTestRunning(aMemory))
-        {
-            int _6000 = aMemory.readByte(0x6000);
-            
-            if(_6000 == 0x81)
-            {
-                // Test needs reset after at least 100ms
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isTestFinished(Memory aMemory)
-    {
-        if(isTestRunning(aMemory))
-        {
-            int _6000 = aMemory.readByte(0x6000);
-            
-            if(_6000 < 0x80)
-            {
-                return true;
-            }            
-        }
-        
-        return false;
-    }
-    
-    private boolean isTestRunning(Memory aMemory)
-    {
-        int _6001 = aMemory.readByte(0x6001);
-        int _6002 = aMemory.readByte(0x6002);
-        int _6003 = aMemory.readByte(0x6003);
-        
-        return _6001 == 0xDE && _6002 == 0xB0 && _6003 == 0x61;
-    }
-    
-    private String getNullTerminatedString(Memory aMemory, int aStartAddress)
-    {
-        StringBuilder _builder = new StringBuilder();
-        
-        int _byte;
-        
-        while((_byte = aMemory.readByte(aStartAddress++)) != 0x00)
-        {
-            _builder.append((char) _byte);
-        }
-        
-        return _builder.toString();
+			@Override
+			public void testFailedWithError(String aMessage, int aStatus) 
+			{
+			}
+			
+			@Override
+			public void testCompletedSuccessfully(String aString) 
+			{
+				System.out.println(aString);
+			}
+			
+			@Override
+			public void println(String aString) 
+			{			
+			}
+		});        
     }
 }
--- src/java/test/org/perturbed/nesjs/core/instructions/BlarggTestCase.java
+++ src/java/test/org/perturbed/nesjs/core/instructions/BlarggTestCase.java
@@ -1,108 +1,46 @@
 package org.perturbed.nesjs.core.instructions;
 
-import java.io.*;
+import java.io.IOException;
 
-import org.junit.*;
-import org.perturbed.nesjs.core.*;
-import org.perturbed.nesjs.core.client.*;
+import junit.framework.Assert;
+
+import org.perturbed.nesjs.core.ResourceROMLoader;
+import org.perturbed.nesjs.core.client.BlarggTestROM;
+import org.perturbed.nesjs.core.client.BlarggTestROM.TestLogger;
+import org.perturbed.nesjs.core.client.ROM;
+import org.perturbed.nesjs.core.client.Utils;
 
 public class BlarggTestCase
 {
      public void runTestROM(String aROMName) throws IOException
      {
-         ROM _rom = ResourceROMLoader.loadROMResource(this.getClass(), aROMName);
-         
-         Memory _memory = _rom.toMemory();
-         
-         MOS6502 _6502 = new MOS6502(_memory);
-         
-         boolean _hasTestResetCPU = false;
-         
-         do
-         {
-             _6502.step();
-             
-             if(isTestNeedsReset(_memory) && !_hasTestResetCPU)
-             {
-                 _hasTestResetCPU = true;
-                 
-                 String _message = getNullTerminatedString(_memory, 0x6004);
-                 System.out.println(_message);
-                 
-                 // Reset after 100ms, so just spin the CPU for a bit
-                 for(int _index = 0; _index < 10000; _index++)
-                 {
-                     _6502.step();
-                 }
-                 
-                 _6502.reset();
-                 System.out.println("RESET");
-             }            
-         }
-         while(!isTestFinished(_memory));
-         
-         String _message = getNullTerminatedString(_memory, 0x6004);
-         System.out.println(_message);
-         
-         Assert.assertEquals(0x00, _memory.readByte(0x6000));
-         Assert.assertTrue(_message.contains("Passed"));
+    	 ROM _rom = ResourceROMLoader.loadROMResource(this.getClass(), aROMName);
+    	 
+    	 BlarggTestROM _romWrapper = new BlarggTestROM(_rom);
+    	 
+    	 _romWrapper.runTestToCompletion(new TestLogger() 
+    	 {		
+    		@Override
+ 			public void println(String aString) 
+    		{
+ 				System.out.println(aString);
+ 			}
+    		 
+    		 @Override
+			public void testFailedWithError(String aMessage, int aStatus) 
+			{
+				System.out.println(aMessage);
+
+				Assert.fail("Test failure, invalid status code [" + Utils.toHexString(aStatus) + "]");
+			}
+			
+			@Override
+			public void testCompletedSuccessfully(String aString) 
+			{
+				System.out.println(aString);
+				
+				Assert.assertTrue(aString.contains("Passed"));
+			}			
+		});
      }
- 
-//-----------------------------------
-// Private methods
-//-----------------------------------     
-     
-     private boolean isTestNeedsReset(Memory aMemory)
-     {
-         if(isTestRunning(aMemory))
-         {
-             int _6000 = aMemory.readByte(0x6000);
-             
-             if(_6000 == 0x81)
-             {
-                 // Test needs reset after at least 100ms
-                 return true;
-             }
-         }
-         
-         return false;
-     }
-     
-     private boolean isTestFinished(Memory aMemory)
-     {
-         if(isTestRunning(aMemory))
-         {
-             int _6000 = aMemory.readByte(0x6000);
-             
-             if(_6000 < 0x80)
-             {
-                 return true;
-             }            
-         }
-         
-         return false;
-     }
-     
-     private boolean isTestRunning(Memory aMemory)
-     {
-         int _6001 = aMemory.readByte(0x6001);
-         int _6002 = aMemory.readByte(0x6002);
-         int _6003 = aMemory.readByte(0x6003);
-         
-         return _6001 == 0xDE && _6002 == 0xB0 && _6003 == 0x61;
-     }
-     
-     private String getNullTerminatedString(Memory aMemory, int aStartAddress)
-     {
-         StringBuilder _builder = new StringBuilder();
-         
-         int _byte;
-         
-         while((_byte = aMemory.readByte(aStartAddress++)) != 0x00)
-         {
-             _builder.append((char) _byte);
-         }
-         
-         return _builder.toString();
-     }
-}
+ }
