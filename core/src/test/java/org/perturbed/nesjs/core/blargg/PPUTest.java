7bb8237784e15648920fb1147c8d5dffdb102f53
matt-preston <matt.r.preston@gmail.com>
2012-09-22 11:54:06 +0100

    Added another set of test roms

---
+++ core/src/test/java/org/perturbed/nesjs/core/blargg/PPUTest.java
@@ -0,0 +1,48 @@
+package org.perturbed.nesjs.core.blargg;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class PPUTest extends BlarggTestROMTest
+{
+    @Override
+    public String getROMRootPath()
+    {
+        return "/ppu_tests/";
+    }
+
+    @Test
+    @Ignore("Don't know how to handle 1 CHR ROM units")
+    public void testPaletteRAM() throws Exception
+    {
+        runTestROM("palette_ram.nes");
+    }
+
+    @Test
+    @Ignore("Don't know how to handle 1 CHR ROM units")
+    public void testPowerUpPalette() throws Exception
+    {
+        runTestROM("power_up_palette.nes");
+    }
+
+    @Test
+    @Ignore("Don't know how to handle 1 CHR ROM units")
+    public void testSpriteRAM() throws Exception
+    {
+        runTestROM("sprite_ram.nes");
+    }
+
+    @Test
+    @Ignore("Don't know how to handle 1 CHR ROM units")
+    public void testVBLClearTime() throws Exception
+    {
+        runTestROM("vbl_clear_time.nes");
+    }
+
+    @Test
+    @Ignore("Don't know how to handle 1 CHR ROM units")
+    public void testVRAMAccess() throws Exception
+    {
+        runTestROM("vram_access.nes");
+    }
+}
