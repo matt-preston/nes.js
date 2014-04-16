578caebe58296ac2ea5872d9ffe919053c5bc1ae
matt-preston <matt.r.preston@gmail.com>
2014-04-16 18:07:35 +0100

    Updated to v5 of the instructions tests

--- core/src/test/java/org/perturbed/nesjs/core/blargg/InstructionsTest.java
+++ core/src/test/java/org/perturbed/nesjs/core/blargg/InstructionsTest.java
@@ -6,81 +6,86 @@ public class InstructionsTest extends BlarggTestROMTest {
 
   @Override
   public String getROMRootPath() {
-    return "/instr_test-v3/rom_singles/";
+    return "/instr_test-v5/rom_singles/";
   }
 
   @Test
-  public void test01Implied() throws Exception {
-    runTestROM("01-implied.nes");
+  public void test01Basics() throws Exception {
+    runTestROM("01-basics.nes");
   }
 
   @Test
-  public void test02Immediate() throws Exception {
-    runTestROM("02-immediate.nes");
+  public void test02Implied() throws Exception {
+    runTestROM("02-implied.nes");
   }
 
   @Test
-  public void test03ZeroPage() throws Exception {
-    runTestROM("03-zero_page.nes");
+  public void test03Immediate() throws Exception {
+    runTestROM("03-immediate.nes");
   }
 
   @Test
-  public void test04ZeroPageXY() throws Exception {
-    runTestROM("04-zp_xy.nes");
+  public void test04ZeroPage() throws Exception {
+    runTestROM("04-zero_page.nes");
   }
 
   @Test
-  public void test05Absolute() throws Exception {
-    runTestROM("05-absolute.nes");
+  public void test05ZeroPageXY() throws Exception {
+    runTestROM("05-zp_xy.nes");
   }
 
   @Test
-  public void test06AbsoluteXY() throws Exception {
-    runTestROM("06-abs_xy.nes");
+  public void test06Absolute() throws Exception {
+    runTestROM("06-absolute.nes");
   }
 
   @Test
-  public void test07IndirectX() throws Exception {
-    runTestROM("07-ind_x.nes");
+  public void test07AbsoluteXY() throws Exception {
+    runTestROM("07-abs_xy.nes");
   }
 
   @Test
-  public void test08IndirectY() throws Exception {
-    runTestROM("08-ind_y.nes");
+  public void test08IndirectX() throws Exception {
+    runTestROM("08-ind_x.nes");
   }
 
   @Test
-  public void test09Branches() throws Exception {
-    runTestROM("09-branches.nes");
+  public void test09IndirectY() throws Exception {
+    runTestROM("09-ind_y.nes");
   }
 
   @Test
-  public void test10Stack() throws Exception {
-    runTestROM("10-stack.nes");
+  public void test10Branches() throws Exception {
+    runTestROM("10-branches.nes");
   }
 
   @Test
-  public void test11JumpJsr() throws Exception {
-    runTestROM("11-jmp_jsr.nes");
+  public void test11Stack() throws Exception {
+    runTestROM("11-stack.nes");
   }
 
   @Test
-  public void test12Rts() throws Exception {
-    runTestROM("12-rts.nes");
+  public void test12JumpJsr() throws Exception {
+    runTestROM("12-jmp_jsr.nes");
   }
 
   @Test
-  public void test13Rti() throws Exception {
-    runTestROM("13-rti.nes");
+  public void test13Rts() throws Exception {
+    runTestROM("13-rts.nes");
   }
 
   @Test
-  public void test14Brk() throws Exception {
-    runTestROM("14-brk.nes");
+  public void test14Rti() throws Exception {
+    runTestROM("14-rti.nes");
   }
 
   @Test
-  public void test15Special() throws Exception {
-    runTestROM("15-special.nes");
+  public void test15Brk() throws Exception {
+    runTestROM("15-brk.nes");
+  }
+
+  @Test
+  public void test16Special() throws Exception {
+    runTestROM("16-special.nes");
   }
 }
